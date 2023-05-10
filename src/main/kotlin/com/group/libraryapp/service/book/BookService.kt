76c5918094eab4d791032ff00bfe5dcd9d0191e8
Slowth-KIM <rt.slowth@gmail.com>
2023-05-10 21:40:13 +0900

    feat(): 세 번째 요구사항 추가하기 - 책 통계
    34강. 다양한 SQL 을 알아보자!
    35강. 애플리케이션 대신 DB 로 기능 구현하기
    36강. 세 번째 요구사항 클리어!

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -44,13 +44,16 @@ class BookService (
 
     @Transactional(readOnly = true)
     fun countLoanedBook(): Int {
-        return userLoanHistoryRepository.findAllByStatus(UserLoanStatus.LOANED).size
+        return userLoanHistoryRepository.countByStatus(UserLoanStatus.LOANED).toInt()
     }
 
     @Transactional(readOnly = true)
     fun getBookStatics(): List<BookStatResponse> {
+/*
         return bookRepository.findAll() // List<Book>
             .groupBy { book -> book.type } // Map<BookType, List<Book>>
             .map { (type, books) -> BookStatResponse(type, books.size) } // List<BookStatResponse>
+*/
+        return bookRepository.getStats()
     }
 }
